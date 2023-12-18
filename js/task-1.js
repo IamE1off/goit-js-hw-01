f822ee6f6403e3e2b39e1b6fd207ec53dcbf7036
IamE1off <132561973+IamE1off@users.noreply.github.com>
2023-12-18 08:16:30 +0100

    update recommendation mentor

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 "use strict";
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  return message;
+
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
